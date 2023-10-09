8efc12d155b07df37f28acffa339a550978d54f8
wach0001 <wach0001@edu.mercantec.dk>
2023-10-09 19:29:13 +0200

    u

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 let banan = 0 
 function minFunktion() {
-    banan += 1
-    document.getElementById("demo").innerHTML = banan;
-   console.log(`${banan}` + 'bananer')
-   
+    banan += 1 
+    if(banan === 1) {
+        document.getElementById("demo").innerHTML = banan + ' banan' ;
+    }
+   else { 
+        document.getElementById("demo").innerHTML = banan + ' bananer' 
+   }
+
   }
   
+  let monkeycost = 20 
+  let monkey = 0 
+
+  function buyMonkey () {
+    if (banan >= monkeycost) {
+        banan = banan - monkeycost
+        document.getElementById("demo").innerHTML = banan + ' bananer'
+    }
+
+    //husk monkeypic 
+  }
+
+ 
